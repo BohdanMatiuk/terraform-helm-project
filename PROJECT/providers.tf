19134fd663bfd2bdf86e40931677b4c0995e4f2f
BohdanMatiuk <mbs.dvp@gmail.com>
2024-12-07 19:30:39 +0000

    aks-project

---
+++ PROJECT/providers.tf
@@ -0,0 +1,30 @@
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+resource "helm_release" "aks_project" {
+  name       = "chart"
+  repository = ".."
+  chart      = "nginx"
+  version    = "1.16.0"
+}
+
+
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+#       command     = "aws"
+#     }
+#   }
+# }
